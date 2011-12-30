d40540d5b70098d80c1fb4c41dbc3f06a2dd3141
Paul McCallick <paul@mccallick.com>
2011-12-30 09:03:08 -0800

    tests for rounding

--- test/unit/org/agilereview/ReviewResultTest.groovy
+++ test/unit/org/agilereview/ReviewResultTest.groovy
@@ -1,5 +1,7 @@
 package org.agilereview
 
+import grails.test.*
+
 /**
  * Created by IntelliJ IDEA.
  * User: paulm
@@ -7,5 +9,35 @@ package org.agilereview
  * Time: 8:30 AM
  * To change this template use File | Settings | File Templates.
  */
-class ReviewResultTest {
+class ReviewResultTest extends GrailsUnitTestCase{
+
+    void test_results_are_rounded_correctly(){
+        def res = new ReviewResult()
+        res.maxAnswer   = 0.333333333
+        res.minAnswer   = 0.333333333
+        res.roleAverage = 0.333333333
+        res.teamAverage = 0.333333333
+        res.yourScore   = 0.333333333
+
+        assertEquals res.maxAnswer   ,0.33
+        assertEquals res.minAnswer   ,0.33
+        assertEquals res.roleAverage ,0.33
+        assertEquals res.teamAverage ,0.33
+        assertEquals res.yourScore   ,0.33
+
+        res.maxAnswer   = 0.666666667
+        res.minAnswer   = 0.666666667
+        res.roleAverage = 0.666666667
+        res.teamAverage = 0.666666667
+        res.yourScore   = 0.666666667
+
+        assertEquals res.maxAnswer   ,0.67
+        assertEquals res.minAnswer   ,0.67
+        assertEquals res.roleAverage ,0.67
+        assertEquals res.teamAverage ,0.67
+        assertEquals res.yourScore   ,0.67
+        
+        
+    } 
+    
 }
