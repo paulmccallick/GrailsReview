baaeeb769fcce381d18a8cc0645f360bff373d0c
Paul McCallick <paul@mccallick.com>
2011-12-30 09:02:46 -0800

    Results are now rounded to two decimal places

--- src/groovy/org/agilereview/ReviewResult.groovy
+++ src/groovy/org/agilereview/ReviewResult.groovy
@@ -7,4 +7,22 @@ class ReviewResult {
 	double roleAverage
 	double minAnswer
 	double maxAnswer
+    def precision = 2
+
+    def getYourScore(){
+        yourScore.round(precision)
+    }
+    def getTeamAverage(){
+        yourScore.round(precision)
+    }
+    def getRoleAverage(){
+        yourScore.round(precision)
+    }
+    def getMinAnswer(){
+        yourScore.round(precision)
+    }
+    def getMaxAnswer(){
+        yourScore.round(precision)
+    }
+
 }
---
+++ test/unit/org/agilereview/ReviewResultTest.groovy
@@ -0,0 +1,11 @@
+package org.agilereview
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: paulm
+ * Date: 12/30/11
+ * Time: 8:30 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class ReviewResultTest {
+}
